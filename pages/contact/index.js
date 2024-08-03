f05b2f6b181e17b054e904b6ef83d9e884c5ca49
NiteshCodes7 <workwithnites@gmail.com>
2024-08-03 16:09:46 +0530

    minor bug fixed

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -29,7 +29,7 @@ const Contact = () => {
             <input type="text" placeholder='subject' className='input'/>
             <textarea placeholder='message' className='textarea'></textarea>
             <button className='btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group'>
-              <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>Let&aspos;s talk</span>
+              <span className='group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500'>Let&apos;s talk</span>
               <BsArrowRight className='-translate-y-[120%] opacity-0 group-hover:flex group-hover:translate-y-0 group-hover:opacity-100 group-hover:transition-all duration-300 absolute text-[22px]'/>
             </button>
         </motion.form>
