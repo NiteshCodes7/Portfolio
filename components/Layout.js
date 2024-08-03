596b3e9b39e38a31dcf614721b8ebb7c8c9bd81e
NiteshCodes7 <workwithnites@gmail.com>
2024-08-03 16:05:17 +0530

    ...

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import {Sora} from "next/font/google"
+import {Sora} from "@next/font/google"
 import Nav from "../components/Nav"
 import Header from '../components/Header'
 import TopLeftImg from '../components/TopLeftImg'
